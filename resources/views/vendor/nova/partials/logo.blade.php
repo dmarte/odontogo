4d7c172c5a60143800af8e97ea004caca868af48
Delvi Marte <delvi.marte@gmail.com>
2021-02-19 02:27:26 -0400

    Hot fixes

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -3,9 +3,8 @@ use Illuminate\Support\Facades\Storage
 @endphp
 
 @auth()
-    @php($team = auth()->user()->member->team)
 
-    @if($team->avatar_path)
+    @if(auth()->user()->team->avatar_path)
         <img src="{{ Storage::disk($team->avatar_disk)->url($team->avatar_path)}}" alt="">
     @endif
 @endauth
