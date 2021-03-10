63d2a5da0a588e8a1fd2fa9e3fe67b8719b2e05a
Delvi Marte <dmarte@famartech.com>
2021-03-10 17:46:50 +0000

    Added cast for discount_rate in order to prevent any bug while creating.

--- app/Models/BudgetTransaction.php
+++ app/Models/BudgetTransaction.php
@@ -9,7 +9,8 @@ class BudgetTransaction extends Item
         parent::booted();
         static::addGlobalScope('data.kind', fn($query) => $query->where('data->kind', Document::KIND_INVOICE_BUDGET));
         static::creating(function (BudgetTransaction $transaction) {
-            $transaction->data = array_merge($transaction->data, [
+	    $transaction->discount_rate = (float) $transaction->discount_rate;	
+	    $transaction->data = array_merge($transaction->data, [
                 'kind' => Document::KIND_INVOICE_BUDGET,
             ]);
         });
